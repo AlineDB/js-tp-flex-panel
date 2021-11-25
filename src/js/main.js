57eccd760949ced7913bd799c969d7388a4368ba
AlineDB <71900695+AlineDB@users.noreply.github.com>
2021-11-25 15:24:08 +0100

    init

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+(function (){
+    const oOpen = {
+        init(){
+            document.documentElement.classList.add("js-enabled");
+            this.aPanels = document.querySelectorAll(".panel");
+            for (const ePanel of this.aPanels) {
+                ePanel.addEventListener("click",(event)=>{
+                    event.currentTarget.classList.toggle("open");
+                });
+                ePanel.addEventListener("transitionend",(event)=>{
+                    if (event.propertyName.includes("flex")){
+                        event.currentTarget.classList.toggle("open-active");
+                    }
+                });
+            }
+        }
+    }
+    oOpen.init();
+})();
